9df8f0fbedc922ddf6ad5becf8c519db8509fa9b
Ellie <112726692+elliezub@users.noreply.github.com>
2023-09-28 16:18:32 -0400

    Add village story line and choices

--- Program.cs
+++ Program.cs
@@ -17,3 +17,25 @@ void Start()
     Console.Beep();
     Village();
 }
+
+void Village()
+{
+    Console.WriteLine("In the village you have grown tired of your mundane life. As you've read in storybooks you decide to have an adventure of your own. You see two paths ahead one leading to the forest and one leading to the beach. Do you:");
+    Console.WriteLine("1) Go to the beach");
+    Console.WriteLine("2) Go to the forest");
+    int choice = int.Parse(Console.ReadLine());
+
+    switch (choice)
+    {
+        case 1:
+            Beach();
+            break;
+        case 2:
+            Forest();
+            break;
+        default:
+            Console.WriteLine("Invalid choice.");
+            Village();
+            break;
+    }
+}
