a235bf75c232572fe4a35688bed5a7313c03cee4
Ellie <112726692+elliezub@users.noreply.github.com>
2023-09-28 16:19:41 -0400

    Add forest storyline and choices

--- Program.cs
+++ Program.cs
@@ -65,3 +65,30 @@ void Beach()
             break;
     }
 }
+
+void Forest()
+{
+    Console.WriteLine("As you venture into the forest, you begin to hear an unusual sound. Following it, you discover a small clearing. In the center, you're surprised to find a baby dragon with an injured wing. It looks up, showing a mix of curiosity and concern");
+    Console.WriteLine($"What will you do, {player.Name}?");
+    Console.WriteLine("1) Offer to help the dragon");
+    Console.WriteLine("2) Capture the dragon so you can sell it on the black market");
+    int choice = int.Parse(Console.ReadLine());
+
+    switch (choice)
+    {
+        case 1:
+            player.AddKindnessPoints(15);
+            Console.WriteLine($"You slowly approach, extending a hand to show you mean no harm. At first, the dragon pulls back, hesitant and wary. But sensing your genuine intent, it allows you to come closer. With patience and care, you manage to tend to its injured wing using some herbs and bandages.");
+            becomeDragonFriend();
+            break;
+        case 2:
+            Console.WriteLine($"Seeing an opportunity for riches, you attempt to capture the young dragon. But looks can be deceiving. Despite its size, it's fierce and full of fire. In a swift motion, the dragon releases a burst of flame, engulfing the surroundings. You realize your grave mistake too late as the inferno consumes you, ending your journey abruptly in the heart of the forest.");
+            End();
+            break;
+        default:
+            Console.WriteLine("Invalid choice.");
+            Forest();
+            break;
+    }
+}
+
